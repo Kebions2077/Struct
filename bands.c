32953943b9de1dbd925cfe82d32322b6e276ed77
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-05-14 17:56:27 -0300

    update

---
+++ bands.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+float median1(int n[9]);
+float median2(int n[9]);
+float median3(int n[9]);
+
+float median1(int n[]){
+    return (n[0]+n[1]+n[2]) /3;    
+                        }
+float median2(int n[9]){
+    return (n[3]+n[4]+n[5]) /3;    
+                        }
+float median3(int n[9]){
+    return (n[6]+n[7]+n[8]) /3;    
+                        }
+
+int main (void){
+    int x, n[9];
+    x=0;
+    
+    for (x=0;x<3;x){
+    
+        struct class {
+            char name[30];
+            int n;
+            long int a_number;
+            float median;
+
+
+        }; 
+        struct class alumn[x]; 
+    }
+    
+
+    
+}
+
+
